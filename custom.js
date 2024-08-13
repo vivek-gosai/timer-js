116776fcb268eaf8bde22f2a75fe90059e50bea9
vivek-gosai <167838467+vivek-gosai@users.noreply.github.com>
2024-08-13 08:56:16 +0530

    count-down

--- custom.js
+++ custom.js
@@ -1,5 +1,4 @@
-let timer;
-
+let timelet;
 let userGet = () => {
     let ho = parseInt(prompt("Enter your Hours"));
     let mi = parseInt(prompt("Enter your Minutes"));
@@ -7,35 +6,36 @@ let userGet = () => {
     return (ho * 3600) + (mi * 60) + se;
 }
 function timefunc(value) {
-   return (value < 10) ? "0" + value : value
+    return (value < 10) ? "0" + value : value
+}
+let time = userGet();
+let timea = () => {
+    if (time <= 0) {
+        clearInterval(timelet);
+        return;
+    }
+    time--;
+    document.getElementById("hours").innerHTML = timefunc(Math.floor(time / 3600));
+    document.getElementById("min").innerHTML = timefunc(Math.floor((time % 3600) / 60));
+    document.getElementById("sec").innerHTML = timefunc(Math.floor(time % 60));
 }
 let start = () => {
-    let time = userGet();
-    timer = setInterval(() => {
-        if (time <= 0) {
-            clearInterval(timer);
-            timer = null;
-            return;
-        }
-        time--;
-
-            document.getElementById("hours").innerHTML =timefunc(Math.floor(time / 3600));
-            document.getElementById("min").innerHTML = timefunc(Math.floor((time % 3600) / 60));
-            document.getElementById("sec").innerHTML = timefunc(Math.floor(time % 60));
-    }, 1000);
+    if (timelet) {
+        clearInterval(timelet); // Clear any existing interval
+    }
+    timelet = setInterval(timea, 1000);
 }
-let stop = () => {
-    if (timer) {
-        clearInterval(timer);
-        timer = null;
+const stop = () => {
+    if (timelet) {
+        clearInterval(timelet); // Stop the countdown
     }
 }
-let reset = () => {
-    document.getElementById("hours").innerHTML = "00";
-    document.getElementById("min").innerHTML = "00";
+const reset = () => {
+    time = userGet();  //reset button get value
+    if (timelet) {
+        clearInterval(timelet);
+    }
+    document.getElementById("hou").innerHTML = "00";
+    document.getElementById("min").innerHTML = "00";    //reset value (00 00 00)
     document.getElementById("sec").innerHTML = "00";
-    if (timer) {
-        clearInterval(timer);
-        timer = null;
-    }
 }
